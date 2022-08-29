036aa852fd647b943f9ba6139682ff899b6b6635
Janus Milojevic <janus.milojevic@gmail.com>
2022-08-29 10:25:57 +0200

    integration of the chessboard-element library

---
+++ app.js
@@ -0,0 +1,10 @@
+//import * from 'chessboard-element';
+const board = document.querySelector('chess-board');
+
+document.querySelector('#showPositionBtn').addEventListener('click', () => {
+  console.log('Current position as an Object:');
+  console.log(board.position);
+
+  console.log('Current position as a FEN string:');
+  console.log(board.fen());
+});
