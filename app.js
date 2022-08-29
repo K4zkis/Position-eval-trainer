670e694d051c9d856de5c8a2a4da04b53d034065
Janus Milojevic <janus.milojevic@gmail.com>
2022-08-29 11:47:39 +0200

    integratioon of the chess-js package for movement logic

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 //import * from 'chessboard-element';
+import { Chess } from 'chess.js'
+
 const board = document.querySelector('chess-board');
 
-document.querySelector('#showPositionBtn').addEventListener('click', () => {
+document.querySelector('#showPositionBtn').addEventListener('click', () => { //() => is the same as writing function 
   console.log('Current position as an Object:');
   console.log(board.position);
 
   console.log('Current position as a FEN string:');
   console.log(board.fen());
+}); 
+document.querySelector('#startBtn').addEventListener('click', () => {
+    board.start();
 });
+document.querySelector('#clearBtn').addEventListener('click', () => {
+    board.clear();
+});
+
+fetch('./samplegame.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json.Game.Notation));
